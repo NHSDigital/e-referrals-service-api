dbf60ebba21b307e10d77774f455490b048a5080
jonathanluckcuck-nhs <jonathan.luckcuck@nhs.net>
2021-02-26 14:46:11 +0000

    Added Ping end point

--- sandbox/src/routes/index.js
+++ sandbox/src/routes/index.js
@@ -1,7 +1,8 @@
 const postCreateProfessionalSession = require('./postCreateProfessionalSession')
 const getStatus = require('./getStatus')
 const getHealth = require('./getHealth')
+const getPing = require('./getPing')
 
-const routes = [].concat(postCreateProfessionalSession, getStatus, getHealth)
+const routes = [].concat(postCreateProfessionalSession, getStatus, getHealth, getPing)
 
 module.exports = routes

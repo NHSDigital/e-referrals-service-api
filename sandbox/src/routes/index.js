5db0be5ac52c40150e3537e566007e3e37a1b718
Jack Wainwright <jack.wainwright@bjss.com>
2021-03-04 12:16:01 +0000

    Added create referral request endpoint to sandbox application

--- sandbox/src/routes/index.js
+++ sandbox/src/routes/index.js
@@ -1,8 +1,9 @@
 const postCreateProfessionalSession = require('./postCreateProfessionalSession')
+const postCreateReferralRequest = require('./postCreateReferralRequest')
 const getStatus = require('./getStatus')
 const getHealth = require('./getHealth')
 const getPing = require('./getPing')
 
-const routes = [].concat(postCreateProfessionalSession, getStatus, getHealth, getPing)
+const routes = [].concat(postCreateReferralRequest, postCreateProfessionalSession, getStatus, getHealth, getPing)
 
 module.exports = routes
---
+++ sandbox/src/routes/postCreateReferralRequest.js
@@ -0,0 +1,22 @@
+module.exports = [
+    /**
+     * POST to /v1/STU3/ReferralRequest
+     * Return createReferralRequest.json back to the client 
+     */
+    {
+        method: '*',
+        path: '/v1/STU3/ReferralRequest',
+        handler: (request, h) => {
+            if (request.raw.req.method !== 'POST') {
+                const responseMessage = {
+                    error: `Request method must be POST, not ${request.raw.req.method}`
+                }
+
+                return h.response(responseMessage).code(405)
+            }
+
+            const path = 'createReferralRequest.json'
+            return h.file(path)
+        }
+    }
+]

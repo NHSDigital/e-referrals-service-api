532d640eaed23822450511f2a68a1bc7b73220cb
nhsd-hubert-raich <hubert.raich1@nhs.net>
2021-04-15 16:54:55 +0100

    [ERSSUP-53351]-[JW, AO]-[Creating Open API documentation for the "Create Referral" xAPI]-[HR]

--- sandbox/src/routes/index.js
+++ sandbox/src/routes/index.js
@@ -2,7 +2,8 @@ const postCreateProfessionalSession = require('./postCreateProfessionalSession')
 const getStatus = require('./getStatus')
 const getHealth = require('./getHealth')
 const getPing = require('./getPing')
+const createReferral = require('./createReferral')
 
-const routes = [].concat(postCreateProfessionalSession, getStatus, getHealth, getPing)
+const routes = [].concat(postCreateProfessionalSession, getStatus, getHealth, getPing, createReferral)
 
 module.exports = routes

e5ec21eb8fed0e6ef9f252682ded0e6d03b1c376
nhsd-hubert-raich <hubert.raich1@nhs.net>
2021-05-05 12:32:11 +0000

    [ERSSUP-54531]-[]-[OAS documentation for ERSSUP-54151: APIM User calls e-RS Endpoint to Create Referral and Send for Triage]-[HR]

--- sandbox/src/routes/index.js
+++ sandbox/src/routes/index.js
@@ -3,7 +3,15 @@ const getStatus = require('./getStatus')
 const getHealth = require('./getHealth')
 const getPing = require('./getPing')
 const createReferral = require('./createReferral')
+const createReferralAndSendForTriage = require('./createReferralAndSendForTriage')
 
-const routes = [].concat(postCreateProfessionalSession, getStatus, getHealth, getPing, createReferral)
+const routes = [].concat(
+    postCreateProfessionalSession,
+    getStatus,
+    getHealth,
+    getPing,
+    createReferral,
+    createReferralAndSendForTriage
+)
 
 module.exports = routes

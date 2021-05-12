40b64412d51ba3b672abc518a436e8a9877c9a00
David Wass <david.wass@bjss.com>
2021-05-12 10:57:56 +0000

    [ERSSUP-54526]-[]-[OAS documentation for ERSSUP-54148: APIM User calls e-RS Endpoint to Retrieve Reference Data]-[]

---
+++ sandbox/src/routes/getCodeSystem.js
@@ -0,0 +1,28 @@
+const mockResponseProvider = require('../services/mockResponseProvider');
+const businessFunctionValidator = require('../services/businessFunctionValidator');
+
+module.exports = [
+  /**
+     * Sandbox implementation for getCodeSystem endpoint
+     */
+  {
+    method: 'GET',
+    path: '/STU3/v1/CodeSystem/{codeSystemType}',
+    handler: (request, h) => {
+        
+      const allowedBusinessFunctions = ["REFERRING_CLINICIAN", "REFERRING_CLINICIAN_ADMIN", "SERVICE_PROVIDER_CLINICIAN"];
+      
+      if (!businessFunctionValidator.hasValidBusinessFunction(request, allowedBusinessFunctions)) {
+        return h.response('This endpoint cannot be accessed using business role provided').code(403);
+      }
+
+      var responsePath = mockResponseProvider.getExampleResponseForGetCodeSystem(request.params.codeSystemType);
+      if (responsePath != null) {
+        return h.file(responsePath).code(200);
+      } 
+        
+      return h.response().code(404);
+      
+    }
+  }
+]

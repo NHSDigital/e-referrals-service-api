5292ec666e1f4a94a43d499da6a7aca2f4ec0bf9
nhsd-hubert-raich <hubert.raich1@nhs.net>
2021-05-27 07:52:02 +0000

    [ERSSUP-53955]-[]-[Creating Open API documentation for the "Retrieve Professional Roles" xAPI]-[HR]

--- sandbox/src/services/businessFunctionValidator.js
+++ sandbox/src/services/businessFunctionValidator.js
@@ -2,13 +2,16 @@
 module.exports = {
 
 
-  hasValidBusinessFunction: function (request, allowedBusinessFunctions) {
+  validateBusinessFunction: function (request, h, allowedBusinessFunctions) {
 
     if (request && request.headers["nhsd-ers-business-function"]) {
-      return allowedBusinessFunctions.includes(request.headers["nhsd-ers-business-function"])
+
+      if (!allowedBusinessFunctions.includes(request.headers["nhsd-ers-business-function"])) {
+        return h.response('SANDBOX_ERROR: This endpoint cannot be accessed using the e-RS Business Function provided. Allowed values: ' + allowedBusinessFunctions).code(403);
+      }
     }
 
-    return false;
+    return undefined;
 
   }
 

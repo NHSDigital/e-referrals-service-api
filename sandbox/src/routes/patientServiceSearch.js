0dac02b71b9996e1921d97594fb816a4de8f00e8
nhsd-hubert-raich <hubert.raich1@nhs.net>
2021-05-28 07:51:35 +0000

    updating examples/sandbox scenarios for patient service search and slot search

--- sandbox/src/routes/patientServiceSearch.js
+++ sandbox/src/routes/patientServiceSearch.js
@@ -1,4 +1,5 @@
 const mockResponseProvider = require('../services/mockResponseProvider')
+const businessFunctionValidator = require('../services/businessFunctionValidator')
 
 module.exports = [
   /**
@@ -9,7 +10,15 @@ module.exports = [
     path: '/STU3/v1/HealthcareService/$ers.searchHealthcareServicesForPatient',
     handler: (request, h) => {
 
+      const allowedBusinessFunctions = ["REFERRING_CLINICIAN", "REFERRING_CLINICIAN_ADMIN"]
+
+      const validationResult = businessFunctionValidator.validateBusinessFunction(request, h, allowedBusinessFunctions)
+      if (validationResult) {
+        return validationResult
+      }
+
       var responsePath = mockResponseProvider.getExampleResponseForPatientServiceSearch(request);
+
       if (responsePath != null) {
         return h.file(responsePath).code(200).type("application/fhir+json")
       } else {

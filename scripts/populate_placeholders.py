5e0203a8a72676946338000ef6790c094fa971d1
nhsd-hubert-raich <hubert.raich1@nhs.net>
2021-06-09 10:58:18 +0000

    [ERSSUP-54768]-[JW,AO]-[OAS documentation for ERSSUP-54155: APIM User calls e-RS Endpoint to Retrieve Referral Request]-[HR]

--- scripts/populate_placeholders.py
+++ scripts/populate_placeholders.py
@@ -11,7 +11,7 @@ import sys
 def main():
     """Main entrypoint"""
     substitutes_dict = {
-      "[[HYPERLINK_A005]]": "[A005 - Retrieve referral request](https://developer.nhs.uk/apis/e-Referrals/explore_endpoint_a005.html)",
+      "[[HYPERLINK_A005]]": "[A005 - Retrieve referral request](#api-Default-a005-retrieve-referral-request)",
       "[[HYPERLINK_A006]]": "[A006 - Retrieve attachment](#api-Default-a006-retrieve-attachment)",
       "[[HYPERLINK_A007]]": "[A007 - Retrieve clinical information](https://developer.nhs.uk/apis/e-Referrals/explore_endpoint_a007.html)",
       "[[HYPERLINK_A010]]": "[A010 - Patient service search](#api-Default-a010-patient-service-search)",
@@ -19,6 +19,7 @@ def main():
       "[[HYPERLINK_A012]]": "[A012 - Maintain referral letter](https://developer.nhs.uk/apis/e-Referrals/explore_endpoint_a012.html)",
       "[[HYPERLINK_A015]]": "[A015 - Retrieve appointment slots](#api-Default-a015-retrieve-appointment-slots)",
       "[[HYPERLINK_A016]]": "[A016 - Book or defer appointment](https://developer.nhs.uk/apis/e-Referrals/explore_endpoint_a016.html)",
+      "[[HYPERLINK_A019]]": "[A019 - Generate patient letter](#api-Default-a019-Generate-patient-letter)",
       "[[HYPERLINK_A021]]": "[A021 - Create referral and send for triage](#api-Default-a021-create-referral-request-and-send-for-triage)",
       "[[HYPERLINK_PDS]]": "[Personal Demographic Service (PDS)](https://digital.nhs.uk/developer/api-catalogue/personal-demographics-service-fhir)",
       "[[HYPERLINK_ERS_BUS_FUNCTIONS]]": "[e-RS Business Functions](https://fhir.nhs.uk/CodeSystem/eRS-BusinessFunction-1)"

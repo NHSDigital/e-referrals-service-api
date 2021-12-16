9c607bf0689c8ac840720916682c327a72b1b865
Andrew Clark <andrew.clark@bjss.com>
2021-12-16 11:50:20 +0000

    [ERSSUP-56861]-[AO/acarriedev]-[Add support for testing the eRS ApiM proxy logic]-[AJC]

--- tests/smoke/configuration/config.py
+++
@@ -1,9 +0,0 @@
-from .environment import ENV
-
-# API Details
-ENVIRONMENT = ENV["environment"]
-BASE_URL = f"https://api.service.nhs.uk" if ENVIRONMENT == "prod" else f"https://{ENVIRONMENT}.api.service.nhs.uk"
-STATUS_ENDPOINT_API_KEY = ENV["status_endpoint_api_key"]
-
-# e-RS
-ERS_BASE_PATH = ENV["ers_base_path"]

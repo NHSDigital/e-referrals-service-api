62e475132b32a50ab65196b8c455b8e7d844cbfe
Nigel Mellor <nigel.mellor@nhs.net>
2022-02-15 10:09:34 +0000

    [ERSSUP-59667]-[JW,AT,AC]-[Policy to translate NHSD-eRS-On-Behalf-Of-User-ID header to x-ers-obo-user-id]-[NM]

--- tests/integration/test_headers.py
+++ tests/integration/test_headers.py
@@ -9,12 +9,13 @@ HEADER_USER_ID = 'x-ers-user-id'
 HEADER_X_REQUEST_ID = 'x-request-id'
 HEADER_ASID = 'xapi_asid'
 
-CORRELATION_ID = "CORRELATION_ID"
-BUSINESS_FUNCTION = "BUSINESS_FUNCTION"
-ODS_CODE = "ODS_CODE"
-FILENAME = "FILENAME"
-COMM_RULE_ORG = "COMM_RULE_ORG"
-REFERRAL_ID = "REFERRAL_ID"
+CORRELATION_ID = 'CORRELATION_ID'
+BUSINESS_FUNCTION = 'BUSINESS_FUNCTION'
+ODS_CODE = 'ODS_CODE'
+FILENAME = 'FILENAME'
+COMM_RULE_ORG = 'COMM_RULE_ORG'
+REFERRAL_ID = 'REFERRAL_ID'
+OBO_USER_ID = 'OBO_USER_ID'
 
 EXPECTED_REFERRAL_ID = '000000040032'
 EXPECTED_CORRELATION_ID = '123123-123123-123123-123123'
@@ -24,48 +25,52 @@ EXPECTED_FILENAME = 'mysuperfilename.txt'
 EXPECTED_ASID = '280477200122'
 EXPECTED_COMM_RULE_ORG = 'R100'
 EXPECTED_USERID = '0987654321123'
+EXPECTED_OBO_USER_ID = '0123456789000'
 
 ORIGINAL_HEADERS = {
-    CORRELATION_ID : "x-correlation-id",
-    BUSINESS_FUNCTION: "nhsd-ers-business-function",
-    ODS_CODE : "nhsd-ers-ods-code",
-    FILENAME : "nhsd-ers-file-name",
-    COMM_RULE_ORG : "nhsd-ers-comm-rule-org",
-    REFERRAL_ID : "nhsd-ers-referral-id"
+    CORRELATION_ID : 'x-correlation-id',
+    BUSINESS_FUNCTION : 'nhsd-ers-business-function',
+    ODS_CODE : 'nhsd-ers-ods-code',
+    FILENAME : 'nhsd-ers-file-name',
+    COMM_RULE_ORG : 'nhsd-ers-comm-rule-org',
+    REFERRAL_ID : 'nhsd-ers-referral-id',
+    OBO_USER_ID : 'nhsd-ers-on-behalf-of-user-id'
 }
 
 RENAMED_HEADERS = {
-    CORRELATION_ID : "nhsd-correlation-id",
-    BUSINESS_FUNCTION : "x-ers-business-function",
-    ODS_CODE : "x-ers-ods-code",
-    FILENAME : "x-ers-xapi-meta-file_name",
-    COMM_RULE_ORG : "x-ers-xapi-comm-rule-org",
-    REFERRAL_ID : "x-ers-xapi-meta-intended_ubrn"
+    CORRELATION_ID : 'nhsd-correlation-id',
+    BUSINESS_FUNCTION : 'x-ers-business-function',
+    ODS_CODE : 'x-ers-ods-code',
+    FILENAME : 'x-ers-xapi-meta-file_name',
+    COMM_RULE_ORG : 'x-ers-xapi-comm-rule-org',
+    REFERRAL_ID : 'x-ers-xapi-meta-intended_ubrn',
+    OBO_USER_ID : 'x-ers-obo-user-id'
 }
+
 
 @pytest.mark.integration_test
 class TestHeaders:
-
-    @pytest.mark.parametrize('user_id,asid', [(EXPECTED_USERID,EXPECTED_ASID)])
+    @pytest.mark.parametrize('user_id,asid', [(EXPECTED_USERID, EXPECTED_ASID)])
     def test_headers(self, access_code, service_url):
         client_request_headers = {
             HEADER_ECHO : '', # enable echo target
             HEADER_AUTHORIZATION : 'Bearer ' + access_code,
-            HEADER_X_REQUEST_ID: 'DUMMY-VALUE',
+            HEADER_X_REQUEST_ID : 'DUMMY-VALUE',
             ORIGINAL_HEADERS[REFERRAL_ID] : EXPECTED_REFERRAL_ID,
             ORIGINAL_HEADERS[CORRELATION_ID] : EXPECTED_CORRELATION_ID,
             ORIGINAL_HEADERS[BUSINESS_FUNCTION] : EXPECTED_BUSINESS_FUNCTION,
             ORIGINAL_HEADERS[ODS_CODE] : EXPECTED_ODS_CODE,
             ORIGINAL_HEADERS[FILENAME] : EXPECTED_FILENAME,
-            ORIGINAL_HEADERS[COMM_RULE_ORG] : EXPECTED_COMM_RULE_ORG}
+            ORIGINAL_HEADERS[COMM_RULE_ORG] : EXPECTED_COMM_RULE_ORG,
+            ORIGINAL_HEADERS[OBO_USER_ID] : EXPECTED_OBO_USER_ID
+        }
 
         # Make the API call
         response = requests.get(service_url, headers=client_request_headers)
-        assert response.status_code == 200, "Expected a 200 when accesing the api but got " + (str)(response.status_code)
+        assert response.status_code == 200, 'Expected a 200 when accesing the api but got ' + (str)(response.status_code)
 
         json_response = response.json()
         target_request_headers = json_response['headers']
-
 
         # Verify the header values
         for header in ORIGINAL_HEADERS:
@@ -82,6 +87,7 @@ class TestHeaders:
         assert target_request_headers[RENAMED_HEADERS[ODS_CODE]] == EXPECTED_ODS_CODE
         assert target_request_headers[RENAMED_HEADERS[FILENAME]] == EXPECTED_FILENAME
         assert target_request_headers[RENAMED_HEADERS[COMM_RULE_ORG]] == EXPECTED_COMM_RULE_ORG
+        assert target_request_headers[RENAMED_HEADERS[OBO_USER_ID]] == EXPECTED_OBO_USER_ID
 
         assert target_request_headers[HEADER_ASID] == EXPECTED_ASID
         assert target_request_headers[HEADER_USER_ID] == EXPECTED_USERID

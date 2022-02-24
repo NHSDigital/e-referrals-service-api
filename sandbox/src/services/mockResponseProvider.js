0491f442c98298bd92de8c0417fcfbbf67d93f39
Jamie Green <jamie.green@bjss.com>
2022-02-24 11:11:13 +0000

    [ERSSUP-59881]-[DW]-[Appointment Non-attendance reason refdata (i.e. DNA reasons) renamed APPOINTMENT-NON-ATTENDANCE to APPOINTMENT-NON-ATTENDANCE-REASON]-[JG]

--- sandbox/src/services/mockResponseProvider.js
+++ sandbox/src/services/mockResponseProvider.js
@@ -97,7 +97,7 @@ module.exports = {
       'CLINIC-TYPE': 'getCodeSystem/responses/ClinicTypeCodeSystem.json',
       'APPOINTMENT-CANCELLATION-REASON': 'getCodeSystem/responses/AppointmentCancellationReasonCodeSystem.json',
       'REFERRAL-CANCELLATION-REASON': 'getCodeSystem/responses/ReferralCancellationReasonCodeSystem.json',
-      'APPOINTMENT-NON-ATTENDANCE': 'getCodeSystem/responses/AppointmentNonAttendanceCodeSystem.json'
+      'APPOINTMENT-NON-ATTENDANCE-REASON': 'getCodeSystem/responses/AppointmentNonAttendanceReasonCodeSystem.json'
     };
 
     return mapExampleGetResponse(request, exampleResponseMap);

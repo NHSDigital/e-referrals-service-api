9fab16efaa816dc3ce0ee1e0d3301a4704b5c1bd
Francisco Videira <francisco.videira@nhs.net>
2022-04-12 22:06:52 +0000

    [ERSSUP-61520]-[JW/AO]-[OAS Modelling Search for ServiceRequest]-[FV]

--- sandbox/src/routes/index.js
+++ sandbox/src/routes/index.js
@@ -32,6 +32,8 @@ const changeShortlist = require('./changeShortlist')
 const changeShortlistAndSendForTriage = require('./changeShortlistAndSendForTriage')
 const cancelReferral = require('./cancelReferral')
 const retrieveAppointment = require('./retrieveAppointment')
+const searchServiceRequest = require('./searchServiceRequest')
+
 
 const routes = [].concat(
   getStatus,
@@ -67,7 +69,8 @@ const routes = [].concat(
   availableActionsForUserList,
   cancelAppointmentActionLater,
   cancelReferral,
-  retrieveAppointment
+  retrieveAppointment,
+  searchServiceRequest
 )
 
 module.exports = routes

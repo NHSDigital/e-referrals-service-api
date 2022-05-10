9d775eb615a7ce8b5f67bd915986db8e27a47988
Francisco Videira <francisco.videira@nhs.net>
2022-05-10 13:37:00 +0000

    [ERSSUP-61674]-[JW/AO]-[Retrieve Binary R4 API design]-[FV]

---
+++ sandbox/src/routes/r4/retrieveBinaryHelper.js
@@ -0,0 +1,25 @@
+module.exports = [
+  /**
+   * Sandbox implementation for retrieveBinary (R4) endpoint helper, 
+   * with an ObjectStore 'mock', allowing redirection and example file retrieval.
+   */
+  {
+    method: 'GET',
+    path: '/ObjectStore/RetrieveBinary/{fileDownloadUuid}',
+    handler: (request, h) => {
+
+      const uuid = request.params.fileDownloadUuid
+      const exampleResponsePath = 'retrieveAttachment/responses/example_attachment.pdf'
+      const filename = 'example_attachment.pdf'
+      const responseCode = 200
+
+      if (uuid === 'd497bbe3-f88b-45f1-b3d4-9c563e4c0f5f') {
+        return h.file(exampleResponsePath, {
+          mode: 'attachment',
+          filename: filename,
+          etagMethod: false
+        }).code(responseCode);
+      }
+    }
+  }
+]

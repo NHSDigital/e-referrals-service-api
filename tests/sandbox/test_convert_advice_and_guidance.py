5d1a1936b159aa200ffaca1eea329b36b795228e
nhsd-david-wass <david.wass@bjss.com>
2022-05-24 14:42:51 +0000

    [ERSSUP-61498]-[]-[Advice And Guidance Sandbox Tests]-[DMW]

---
+++ tests/sandbox/test_convert_advice_and_guidance.py
@@ -0,0 +1,73 @@
+from typing import Callable, Dict, Iterable, List
+from urllib import response
+
+import pytest
+import asserts
+
+from requests import Response
+from SandboxTest import SandboxTest
+from data import Actor
+from utils import HttpMethod
+
+
+@pytest.mark.sandbox
+class TestConvertAdviceAndGuidance(SandboxTest):
+    authorised_actor_data = [Actor.SPC, Actor.SPCA]
+
+    allowed_business_function_data = [
+        "SERVICE_PROVIDER_CLINICIAN",
+        "SERVICE_PROVIDER_CLINICIAN_ADMIN",
+    ]
+
+    testdata = [
+        (
+            "convertAdviceAndGuidanceToReferral/requests/NoAttachments.json",
+            "convertAdviceAndGuidanceToReferral/responses/NoAttachments.json",
+        ),
+        (
+            "convertAdviceAndGuidanceToReferral/requests/WithAttachments.json",
+            "convertAdviceAndGuidanceToReferral/responses/WithAttachments.json",
+        ),
+    ]
+
+    @pytest.fixture
+    def endpoint_url(self) -> str:
+        return (
+            "FHIR/STU3/ReferralRequest/$ers.createFromCommunicationRequestActionLater"
+        )
+
+    @pytest.fixture
+    def authorised_actors(self) -> Iterable[Actor]:
+        return TestConvertAdviceAndGuidance.authorised_actor_data
+
+    @pytest.fixture
+    def allowed_business_functions(self) -> Iterable[str]:
+        return TestConvertAdviceAndGuidance.allowed_business_function_data
+
+    @pytest.fixture
+    def call_endpoint(
+        self, call_endpoint_url_with_request: Callable[[Actor, str], Response],
+    ) -> Callable[[Actor], Response]:
+        return lambda actor, headers={}: call_endpoint_url_with_request(
+            actor,
+            "convertAdviceAndGuidanceToReferral/requests/NoAttachments.json",
+            headers,
+        )
+
+    @pytest.mark.parametrize("actor", authorised_actor_data)
+    @pytest.mark.parametrize("requestJson,response", testdata)
+    def test_success(
+        self,
+        call_endpoint_url_with_request: Callable[[Actor, str], Response],
+        load_json: Callable[[str], Dict[str, str]],
+        actor: Actor,
+        requestJson,
+        response,
+    ):
+        expected_response = load_json(response)
+        actual_response = call_endpoint_url_with_request(actor, requestJson)
+
+        asserts.assert_status_code(201, actual_response.status_code)
+        asserts.assert_response(expected_response, actual_response)
+
+        asserts.assert_headers(actual_response)

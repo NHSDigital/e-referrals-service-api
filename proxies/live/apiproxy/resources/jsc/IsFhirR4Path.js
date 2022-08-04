29d6547c3a7bfb6f9f4f914c053196f218c7f17a
Angel Pastor <angel.pastor@bjss.com>
2022-08-04 08:26:35 +0000

    [ERSSUP-63312]-[FV/JW]-[FHIR R4 OperationOutcome for 401]-[AP]

--- proxies/live/apiproxy/resources/jsc/IsFhirR4Path.js
+++ proxies/live/apiproxy/resources/jsc/IsFhirR4Path.js
@@ -1,8 +1,6 @@
 const proxy_path = context.getVariable('proxy.pathsuffix');
 const regex_fhir_r4 = /.*\/FHIR\/R4\/.*/;
-const regex_fhir_r4_v1 = /.*\/R4\/v1\/.*/;
 const found_fhir_r4 = proxy_path.match(regex_fhir_r4);
-const found_fhir_r4_v1 = proxy_path.match(regex_fhir_r4_v1);
-const is_fhir_r4_path = (found_fhir_r4 || found_fhir_r4_v1) ? true : false;
+const is_fhir_r4_path = found_fhir_r4 ? true : false;
 
 context.setVariable("isFhirR4Path", is_fhir_r4_path);

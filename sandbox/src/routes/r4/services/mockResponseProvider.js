7507ba6f6833cf2ec5e71fb927da35a60390fb97
Francisco Videira <francisco.videira@nhs.net>
2022-09-27 20:00:07 +0000

    Add request parameters filename and fileSize

--- sandbox/src/routes/r4/services/mockResponseProvider.js
+++ sandbox/src/routes/r4/services/mockResponseProvider.js
@@ -1,3 +1,31 @@
+const fs = require('fs')
+const lodash = require('lodash')
+
+function mapExampleResponse(request, exampleResponseMap) {
+
+  if (request && request.payload) {
+    for (const [requestBodyPath, response] of Object.entries(exampleResponseMap)) {
+      try {
+        const exampleRequestBody = JSON.parse(fs.readFileSync(requestBodyPath))
+        var requestBody = request.payload;
+        if ('object' != (typeof requestBody)) {
+          requestBody = JSON.parse(request.payload)
+        }
+
+        if (lodash.isEqual(requestBody, exampleRequestBody)) {
+          return response;
+        }
+      } catch (err) {
+        console.error(err)
+        throw err
+      }
+    }
+  }
+
+  return null;
+}
+
+
 module.exports = {
 
   getExampleResponseForRetrieveBusinessFunctions: function () {
@@ -63,7 +91,10 @@ module.exports = {
   getExampleResponseForRequestUploadUri: function (request) {
     const id = request.params.id
     if (id === 'r.f6dc823a-e673-4f74-9edc-a49525edd2a5') {
-      return 'r4/requestUploadUri/responses/ResponseExample.json'
+      var responseMap = {
+        'src/mocks/r4/requestUploadUri/requests/RequestExample.json': 'r4/requestUploadUri/responses/ResponseExample.json'
+      }
+      return mapExampleResponse(request, responseMap)
     }
   }
 }
---
+++ tests/sandbox/r4/test_a039_generate_upload_uri.py
@@ -0,0 +1,77 @@
+from typing import Callable, Dict, Iterable, List
+from urllib import response
+
+import pytest
+import asserts
+
+from requests import Response
+from SandboxTest import SandboxTest
+from data import Actor
+from utils import HttpMethod
+
+
+@pytest.mark.sandbox
+class TestGenerateUploadUri(SandboxTest):
+
+    authorised_actor_data = [Actor.RC, Actor.RC_DEV, Actor.RCA, Actor.SPC, Actor.SPCA]
+
+    allowed_business_function_data = [
+        "REFERRING_CLINICIAN",
+        "REFERRING_CLINICIAN_ADMIN",
+        "SERVICE_PROVIDER_CLINICIAN",
+        "SERVICE_PROVIDER_CLINICIAN_ADMIN",
+    ]
+
+    testdata = [
+        (
+            "r4/requestUploadUri/requests/RequestExample.json",
+            "r4/requestUploadUri/responses/ResponseExample.json",
+        ),
+    ]
+
+    @pytest.fixture
+    def endpoint_url(self) -> str:
+        return "FHIR/R4/ServiceRequest/r.f6dc823a-e673-4f74-9edc-a49525edd2a5/$ers.generateUploadURI"
+
+    @pytest.fixture
+    def http_method(self) -> HttpMethod:
+        return HttpMethod.POST
+
+    @pytest.fixture
+    def authorised_actors(self) -> Iterable[Actor]:
+        return TestGenerateUploadUri.authorised_actor_data
+
+    @pytest.fixture
+    def allowed_business_functions(self) -> Iterable[str]:
+        return TestGenerateUploadUri.allowed_business_function_data
+
+    @pytest.fixture
+    def call_endpoint(
+        self,
+        call_endpoint_url_with_request: Callable[
+            [Actor, str, Dict[str, str]], Response
+        ],
+    ) -> Callable[[Actor], Response]:
+        return lambda actor, headers={}: call_endpoint_url_with_request(
+            actor, "r4/requestUploadUri/requests/RequestExample.json", headers,
+        )
+
+    @pytest.mark.parametrize("actor", authorised_actor_data)
+    @pytest.mark.parametrize("requestJson,response", testdata)
+    def test_success(
+        self,
+        call_endpoint_url_with_request: Callable[
+            [Actor, str, Dict[str, str]], Response
+        ],
+        load_json: Callable[[str], Dict[str, str]],
+        actor: Actor,
+        requestJson,
+        response,
+    ):
+        expected_response = load_json(response)
+        actual_response = call_endpoint_url_with_request(actor, requestJson)
+
+        asserts.assert_status_code(200, actual_response.status_code)
+        asserts.assert_response(expected_response, actual_response)
+
+        asserts.assert_json_response_headers(actual_response)

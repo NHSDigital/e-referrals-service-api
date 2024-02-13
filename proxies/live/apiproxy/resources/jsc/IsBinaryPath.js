53ce694deedd950d9eb7c5431e9a58ad2c50ae64
David Wass <david.wass@bjss.com>
2024-02-13 13:09:34 +0000

    [ERSSUP-72719]-[AO/JW]-[Remove Binary endpoint specific rate limiting]-[DMW]

--- proxies/live/apiproxy/resources/jsc/IsBinaryPath.js
+++
@@ -1,7 +0,0 @@
-const proxy_path = context.getVariable('proxy.pathsuffix');
-const regex = /.*\/Binary.*/;
-const found = proxy_path.match(regex);
-
-const is_binary_path = found ? true : false;
-
-context.setVariable("isBinaryPath", is_binary_path);

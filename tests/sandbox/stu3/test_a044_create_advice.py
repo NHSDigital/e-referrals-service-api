556a4c92f8d2359ad2e9dd98c21afc90a012b69c
Jack Wainwright <jack.wainwright@bjss.com>
2024-03-21 12:18:52 +0000

    [ERSSUP-74034]-[]-[Added sandbox integration test for Create Advice endpoint]-[JW]

---
+++ tests/sandbox/stu3/test_a044_create_advice.py
@@ -0,0 +1,66 @@
+from typing import Callable, Dict, Iterable
+
+import pytest
+import asserts
+
+from requests import Response
+
+from SandboxTest import SandboxTest
+from data import Actor
+from utils import HttpMethod
+
+authorised_actor_data = [Actor.RC, Actor.RC_DEV, Actor.RCA]
+
+
+@pytest.mark.sandbox
+class TestCreateAdvice(SandboxTest):
+
+    allowed_business_function_data = [
+        "REFERRING_CLINICIAN",
+        "REFERRING_CLINICIAN_ADMIN",
+    ]
+
+    request_path = "stu3/createAdvice/requests/ExampleAdvice.json"
+
+    @pytest.fixture
+    def endpoint_url(self) -> str:
+        return "FHIR/STU3/CommunicationRequest/$ers.createAdvice"
+
+    @pytest.fixture
+    def http_method(self) -> HttpMethod:
+        return HttpMethod.POST
+
+    @pytest.fixture
+    def authorised_actors(self) -> Iterable[Actor]:
+        return authorised_actor_data
+
+    @pytest.fixture
+    def allowed_business_functions(self) -> Iterable[str]:
+        return self.allowed_business_function_data
+
+    @pytest.fixture
+    def call_endpoint(
+        self,
+        call_endpoint_url_with_request: Callable[
+            [Actor, str, Dict[str, str]], Response
+        ],
+    ) -> Callable[[Actor], Response]:
+        return lambda actor, headers={}: call_endpoint_url_with_request(
+            actor, self.request_path, headers
+        )
+
+    @pytest.mark.parametrize("actor", authorised_actor_data)
+    def test_success(
+        self,
+        call_endpoint_url_with_request: Callable[
+            [Actor, str, Dict[str, str]], Response
+        ],
+        load_json: Callable[[str], Dict[str, str]],
+        actor: Actor,
+    ):
+        expected_response = load_json("stu3/createAdvice/responses/ExampleAdvice.json")
+        actual_response = call_endpoint_url_with_request(actor, self.request_path)
+
+        asserts.assert_status_code(200, actual_response.status_code)
+        asserts.assert_response(expected_response, actual_response)
+        asserts.assert_json_response_headers(actual_response)

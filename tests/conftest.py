c9967630b83b3680f8bae64a0c3786fda5cba078
EdwardWills-nhs <edward.wills1@nhs.net>
2026-02-25 10:13:36 +0000

    [RAA-5119]-[BS]-[Changed back callback]-[EW]

--- tests/conftest.py
+++ tests/conftest.py
@@ -326,7 +326,7 @@ def make_app(client):
             "name": app_name,
             "scopes": [],
             "status": "approved",
-            "callbackUrl": "https://example.com/callback",
+            "callbackUrl": "https://google.com/callback",
         }
 
         app = devAppAPI.create_app(email="apm-testing-internal-dev@nhs.net", body=body)
